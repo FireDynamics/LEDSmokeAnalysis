d089636f9d2972903e84b5cb5440c6742f895172
O-Sven <1860830@uni-wuppertal.de>
2020-11-17 13:41:30 +0100

    room corners, fire and cameras are now plotted in the coordinate plot, if experiment_structure.csv is available

--- ledsa/tests/plot_coordinates.py
+++ ledsa/tests/plot_coordinates.py
@@ -3,25 +3,33 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from ..core.led_helper import load_file
 from os import sep
+import pandas as pd
 
 
 def plot_coordinates():
     leds = load_file('analysis{}led_search_areas_with_coordinates.csv'.format(sep), delim=',')
-    print(np.shape(leds))
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    ax.scatter(leds[:, 3], leds[:, 4], leds[:, 5])
-    c1 = [7.29, 6.46, 2.3]
-    c2 = [1.28, 4.88, 2.3]
-    f = [4.5, 5.25, 0]
-    x = [7.29, 1.28, 4.5]
-    y = [6.46, 4.88, 5.25]
-    z = [2.3, 2.3, 0]
-    ax.scatter(x, y, z, s=30)
-    ax.set_xbound(0, 7)
-    ax.set_ybound(0, 7)
-    ax.set_zbound(0, 7)
+    ax.scatter(leds[:, 3], leds[:, 4], leds[:, 5], c='b', s=10, marker='.')
+
+    try:
+        fire = pd.read_csv('experiment_structure.csv', usecols=lambda x: x.upper() in ['FIRE']).values.tolist()
+        cameras = pd.read_csv('experiment_structure.csv',
+                              usecols=lambda x: x.upper() in ['CAM1', 'CAM2']).values.tolist()
+        corners = pd.read_csv('experiment_structure.csv',
+                              usecols=lambda x: x.upper() in ['CORNER1', 'CORNER2', 'CORNER3', 'CORNER4', 'CORNER5',
+                                                              'CORNER6', 'CORNER7', 'CORNER8']).values.tolist()
+        ax.scatter(fire[0], fire[1], fire[2], s=150, c='r', marker='^')
+        ax.scatter(cameras[0][0], cameras[1][0], cameras[2][0], s=300, c='k', marker='$Cam1$')
+        ax.scatter(cameras[0][1], cameras[1][1], cameras[2][1], s=300, c='k', marker='$Cam2$')
+        ax.scatter(corners[0], corners[1], corners[2], s=40, c='k', marker='+')
+    except IOError:
+        print('experiment_structure.csv not found.')
+        print('Camera position, fire and corners are not plotted.')
+    ax.set_xbound(0, 10)
+    ax.set_ybound(0, 10)
+    ax.set_zbound(0, 4)
 
     plt.show()
 
