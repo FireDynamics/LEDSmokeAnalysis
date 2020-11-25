dba5a7cb7255676d0164decdb939c3847f1b4fbd
O-Sven <1860830@uni-wuppertal.de>
2020-11-25 17:24:37 +0100

    introduce AbsorptionCoefficient Class

---
+++ ledsa/analysis/AbsorptionCoefficients.py
@@ -0,0 +1,77 @@
+from typing import List
+import numpy as np
+import pandas as pd
+from scipy.optimize import minimize
+from ledsa.analysis.Experiment import Experiment, Layers, Camera
+from ledsa.analysis.calculations import read_hdf
+
+
+class AbsorptionCoefficients:
+    def __init__(self, experiment=Experiment(layers=Layers(20, 1.0, 3.35), camera=Camera(pos_x=4.4, pos_y=2, pos_z=2.3),
+                                             led_array=3, channel=0),
+                 reference_property='sum_col_val', num_ref_imgs=10):
+        self.coefficients_per_image_and_layer = []
+        self.calculated_img_data = pd.DataFrame()
+        self.experiment = experiment
+        self.distances_per_led_and_layer = []
+        self.reference_property = reference_property
+        self.num_ref_imgs = num_ref_imgs
+
+    def calc_and_set_coefficients(self) -> None:
+        if len(self.distances_per_led_and_layer) == 0:
+            self.distances_per_led_and_layer = self.calc_distance_array()
+        ref_intensities = self.calc_ref_intensities()
+        kappa0 = np.zeros(self.experiment.layers.amount)
+        bounds = [(0, 10) for _ in range(self.experiment.layers.amount)]
+        for img_id, single_img_data in self.calculated_img_data.groupby(level=0):
+            single_img_array = single_img_data[self.reference_property].to_numpy()
+            rel_intensities = single_img_array / ref_intensities
+
+            res = minimize(self.cost_function, kappa0, args=rel_intensities,
+                           method='TNC', bounds=tuple(bounds),
+                           options={'maxiter': 200, 'gtol': 1e-5, 'disp': True})
+
+            kappa0[:] = res.x
+            self.coefficients_per_image_and_layer.append(res.x)
+        return None
+
+    def save(self) -> None:
+        path = self.experiment.path / 'analysis' / 'AbsorptionCoefficients'
+        if not path.exists():
+            path.mkdir(parents=True)
+        path = path / f'absorption_coefficients_channel_{self.experiment.channel}.csv'
+        np.savetxt(path, self.coefficients_per_image_and_layer, delimiter=',')
+
+    def load_img_data(self) -> None:
+        img_data = read_hdf(self.experiment.channel, path=self.experiment.path)
+        img_data_cropped = img_data['img_id', 'led_id', 'line', self.reference_property]
+        self.calculated_img_data = img_data_cropped[img_data_cropped['line'] == self.experiment.led_array]
+
+    def calc_distance_array(self) -> List[np.ndarray]:
+        distances = []
+        for led in self.experiment.leds:
+            d = self.experiment.calc_traversed_dist_per_layer(led)
+            distances.append(d)
+        return distances
+
+    def calc_intensities(self, kappas: np.ndarray) -> np.ndarray:
+        n_leds = self.experiment.led_number
+        intensities = np.zeros(n_leds)
+        for led in range(n_leds):
+            intensity = 1.0
+            for layer in range(len(self.distances_per_led_and_layer[led])):
+                intensity = intensity * np.exp(-kappas[layer]*self.distances_per_led_and_layer[led][layer])
+            intensities[led] = intensity
+        return intensities
+
+    def cost_function(self, kappas: np.ndarray, target: np.ndarray) -> float:
+        intensities = self.calc_intensities(kappas)
+        rmse = np.sqrt(np.sum((intensities - target) ** 2)) / len(intensities)
+        curvature = np.sum(np.abs(kappas[0:-2] - 2 * kappas[1:-1] + kappas[2:])) * len(intensities) * 2 * 1e-6
+        low_values = - np.sum(kappas) / len(kappas) * 6e-3
+        return rmse + curvature + low_values
+
+    def calc_ref_intensities(self) -> np.ndarray:
+        ref_img_data = self.calculated_img_data.query(f'img_id <= {self.num_ref_imgs}')
+        ref_intensities = ref_img_data.mean(0, level='led_id')
+        return ref_intensities[self.reference_property].to_numpy()
---
+++ ledsa/tests/UnitTests/test_AbsorptionCoefficients.py
@@ -0,0 +1,115 @@
+import tempfile
+from unittest import TestCase, mock
+from unittest.mock import MagicMock
+
+import numpy as np
+import pandas as pd
+from pathlib import Path
+import os
+
+from ledsa.analysis.AbsorptionCoefficients import AbsorptionCoefficients
+from ledsa.analysis.Experiment import Layers, Experiment, Camera, LED
+
+
+class TestAbsorptionCoefficients(TestCase):
+    def setUp(self):
+        layers = Layers(5, 0, 5)
+        self.ac = AbsorptionCoefficients(num_ref_imgs=2, experiment=Experiment(layers=layers, led_array=1,
+                                                                               camera=Camera(2, 0, 2)))
+        self.set_leds()
+        self.set_dist_array()
+        self.set_calculated_img_data()
+
+    def set_leds(self):
+        self.ac.experiment.led_number = 5
+        for i in range(self.ac.experiment.led_number):
+            led = LED(id=i, pos_x=i, pos_y=2, pos_z=i)
+            self.ac.experiment.leds.append(led)
+
+    def set_dist_array(self):
+        for i in range(self.ac.experiment.led_number):
+            self.ac.distances_per_led_and_layer.append(np.ones(self.ac.experiment.layers.amount))
+
+    def set_calculated_img_data(self):
+        index_list = [[1, 2, 3], [0, 1, 2, 3, 4]]
+        index = pd.MultiIndex.from_product(index_list, names=['img_id', 'led_id'])
+        self.ac.calculated_img_data = pd.DataFrame(np.ones([15, 2]), index=index,
+                                                   columns=['line', self.ac.reference_property])
+
+
+class ComputeReferenceIntensitiesTestCase(TestAbsorptionCoefficients):
+    def test_num_of_ref_intensities_are_right(self):
+        ref_intensities = self.ac.calc_ref_intensities()
+        self.assertEqual(self.ac.experiment.led_number, len(ref_intensities))
+
+    def test_ref_intensities_are_right(self):
+        ref_intensities = self.ac.calc_ref_intensities()
+        self.assertAlmostEqual(1, ref_intensities[0])
+
+
+class ComputeIntensitiesTestCase(TestAbsorptionCoefficients):
+    def test_num_of_intensities_are_right(self):
+        kappas = np.zeros(self.ac.experiment.layers.amount)
+        intensities = self.ac.calc_intensities(kappas)
+        self.assertEqual(len(intensities), self.ac.experiment.layers.amount)
+
+    def test_intensities_are_right(self):
+        kappas = np.zeros(self.ac.experiment.layers.amount)
+        intensities = self.ac.calc_intensities(kappas)
+        self.assertEqual(intensities[0], 1)
+
+
+class SaveTestCase(TestAbsorptionCoefficients):
+    def setUp(self):
+        super().setUp()
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.ac.experiment.path = Path(self.temp_dir.name)
+        self.path = self.ac.experiment.path / 'analysis' / 'AbsorptionCoefficients'
+        self.ac.save()
+
+    def test_path_was_created(self):
+        self.assertTrue(self.path.exists())
+
+    def test_file_was_created(self):
+        self.assertEqual(1, len(os.listdir(self.path)))
+
+
+class CostFunctionTestCase(TestAbsorptionCoefficients):
+    def setUp(self):
+        super().setUp()
+        intensities = np.ones(self.ac.experiment.led_number)
+        intensities /= 2
+        self.ac.calc_intensities = MagicMock(return_value=intensities)
+
+    def test_returns_pos_value(self):
+        kappas = np.zeros(self.ac.experiment.layers.amount)
+        target = np.ones(self.ac.experiment.led_number)
+        cost = self.ac.cost_function(kappas, target)
+        self.assertTrue(cost >= 0)
+
+    def test_target_same_as_intensities_returns_cost_smaller_equal_zero(self):
+        kappas = np.zeros(self.ac.experiment.layers.amount)
+        target = np.ones(self.ac.experiment.led_number) / 2
+        cost = self.ac.cost_function(kappas, target)
+        self.assertTrue(cost <= 0)
+
+
+class CalcDistArrayTestCase(TestAbsorptionCoefficients):
+    def test_amount_of_distances_equals_amount_of_layers(self):
+        self.ac.experiment.calc_traversed_dist_per_layer = MagicMock(return_value=np.ones(5))
+        dist = self.ac.calc_distance_array()
+        self.assertEqual(self.ac.experiment.layers.amount, len(dist))
+
+
+class CoefficientCalculationTestCase(TestAbsorptionCoefficients):
+    class MockMinimizeResult:
+        def __init__(self):
+            self.x = np.ones(5)
+
+    @mock.patch('ledsa.analysis.AbsorptionCoefficients.minimize', return_value=MockMinimizeResult())
+    def test_calculated_coefficient_matrix_has_right_dim(self, mocked_minimize):
+        self.ac.calc_and_set_coefficients()
+        self.assertEqual(len(self.ac.calculated_img_data.groupby(level='img_id')),
+                         len(self.ac.coefficients_per_image_and_layer), msg='Test Image Dimension')
+        self.assertEqual(self.ac.experiment.layers.amount, len(self.ac.coefficients_per_image_and_layer[0]),
+                         msg='Test Layer Dim')
