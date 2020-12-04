82894c3da0355f18d7f2c5f3a3c419d5e2fccdb1
O-Sven <1860830@uni-wuppertal.de>
2020-12-04 13:10:23 +0100

    debugging and refactoring

---
+++ ledsa/analysis/ExtinctionCoefficientsAnalytic.py
@@ -0,0 +1,78 @@
+import numpy as np
+from ledsa.analysis.Experiment import Experiment, Layers, Camera, LED
+from ledsa.analysis.ExtinctionCoefficients import ExtinctionCoefficients
+
+
+class ExtinctionCoefficientsAnalytic(ExtinctionCoefficients):
+    def __init__(self, experiment=Experiment(layers=Layers(10, 1.0, 3.35), camera=Camera(pos_x=4.4, pos_y=2, pos_z=2.3),
+                                             led_array=3, channel=0),
+                 reference_property='sum_col_val', num_ref_imgs=10):
+        super().__init__(experiment, reference_property, num_ref_imgs)
+        self.type = 'analytic'
+
+    def calc_mean_dist_per_dummy_led_and_layer(self, mean_led_positions_per_layer: np.ndarray) -> np.ndarray:
+        mean_dist_per_dummy_led_and_layer = np.zeros((self.experiment.layers.amount, self.experiment.layers.amount))
+        for layer in range(self.experiment.layers.amount):
+            led = LED(layer, mean_led_positions_per_layer[layer, 0], mean_led_positions_per_layer[layer, 1],
+                      mean_led_positions_per_layer[layer, 2])
+            mean_dist_per_dummy_led_and_layer[layer] = self.experiment.calc_traversed_dist_per_layer(led)
+        return mean_dist_per_dummy_led_and_layer
+
+    def calc_mean_relative_intensities_per_layer(self, rel_intensities: np.ndarray) -> np.ndarray:
+        mean_rel_intensity_per_layer = np.zeros(self.experiment.layers.amount)
+        for layer in range(self.experiment.layers.amount):
+            led_counter = 0
+            leds_in_layer = 0
+            for led in self.experiment.leds:
+                led_counter += 1
+                if led in self.experiment.layers[layer]:
+                    leds_in_layer += 1
+                    mean_rel_intensity_per_layer[layer] += rel_intensities[led_counter - 1]
+            if leds_in_layer > 0:
+                mean_rel_intensity_per_layer[layer] = mean_rel_intensity_per_layer[layer] / leds_in_layer
+            else:
+                mean_rel_intensity_per_layer[layer] = np.nan
+        return mean_rel_intensity_per_layer
+
+    def calc_mean_led_positions_per_layer(self):
+        mean_led_pos_per_layer = np.zeros((self.experiment.layers.amount, 3))
+        for layer in range(self.experiment.layers.amount):
+            led_counter = 0
+            for led in self.experiment.leds:
+                if led in self.experiment.layers[layer]:
+                    led_counter += 1
+                    mean_led_pos_per_layer[layer] += [led.pos_x, led.pos_y, led.pos_z]
+            if led_counter > 0:
+                mean_led_pos_per_layer[layer] = mean_led_pos_per_layer[layer] / led_counter
+            else:
+                mean_led_pos_per_layer[layer] = np.array([np.nan, np.nan, np.nan])
+        return mean_led_pos_per_layer
+
+    def calc_coefficients_of_img(self, rel_intensities: np.ndarray) -> np.ndarray:
+        mean_led_positions = self.calc_mean_led_positions_per_layer()
+        mean_dist = self.calc_mean_dist_per_dummy_led_and_layer(mean_led_positions)
+        mean_rel_intensity = self.calc_mean_relative_intensities_per_layer(rel_intensities)
+        camera_layer = self.find_camera_layer(mean_dist)
+        kappas = np.zeros(self.experiment.layers.amount)
+
+        for upper_layer in range(camera_layer, self.experiment.layers.amount):
+            kappas[upper_layer] = self.calc_kappa(kappas, upper_layer,
+                                                  mean_dist[upper_layer],
+                                                  mean_rel_intensity[upper_layer])
+        for bottom_layer in range(camera_layer - 1, -1, -1):
+            kappas[bottom_layer] = self.calc_kappa(kappas, bottom_layer,
+                                                   mean_dist[bottom_layer],
+                                                   mean_rel_intensity[bottom_layer])
+        return kappas
+
+    def find_camera_layer(self, mean_dist_per_led_and_layer: np.ndarray) -> int:
+        for layer in range(self.experiment.layers.amount):
+            if np.sum(mean_dist_per_led_and_layer[layer] > 0) == 1:
+                return layer
+
+    def calc_kappa(self, kappas: np.ndarray, layer: int, dist_per_layer: np.ndarray,
+                   rel_intensity: float) -> np.ndarray:
+        if dist_per_layer[layer] == 0:
+            return np.nan
+        kappa_new = (-np.log(rel_intensity) - sum(kappas * dist_per_layer)) / dist_per_layer[layer]
+        return kappa_new
---
+++ ledsa/analysis/ExtinctionCoefficientsNumeric.py
@@ -0,0 +1,42 @@
+import numpy as np
+from ledsa.analysis.Experiment import Experiment, Layers, Camera
+from ledsa.analysis.ExtinctionCoefficients import ExtinctionCoefficients
+from scipy.optimize import minimize
+
+
+class ExtinctionCoefficientsNumeric(ExtinctionCoefficients):
+    def __init__(self, experiment=Experiment(layers=Layers(20, 1.0, 3.35), camera=Camera(pos_x=4.4, pos_y=2, pos_z=2.3),
+                                             led_array=3, channel=0),
+                 reference_property='sum_col_val', num_ref_imgs=10):
+        super().__init__(experiment, reference_property, num_ref_imgs)
+        self.bounds = [(0, 10) for _ in range(self.experiment.layers.amount)]
+
+        self.type = 'numeric'
+
+    def calc_coefficients_of_img(self, rel_intensities: np.ndarray) -> np.ndarray:
+        if len(self.coefficients_per_image_and_layer) == 0:
+            kappa0 = np.zeros(self.experiment.layers.amount)
+        else:
+            kappa0 = self.coefficients_per_image_and_layer[-1]
+        fit = minimize(self.cost_function, kappa0, args=rel_intensities,
+                       method='TNC', bounds=tuple(self.bounds),
+                       options={'maxiter': 200, 'gtol': 1e-5, 'disp': True})
+        kappas = fit.x
+        return kappas
+
+    def calc_intensities(self, kappas: np.ndarray) -> np.ndarray:
+        n_leds = self.experiment.led_number
+        intensities = np.zeros(n_leds)
+        for led in range(n_leds):
+            intensity = 1.0
+            for layer in range(len(self.distances_per_led_and_layer[led, :])):
+                intensity = intensity * np.exp(-kappas[layer]*self.distances_per_led_and_layer[led, layer])
+            intensities[led] = intensity
+        return intensities
+
+    def cost_function(self, kappas: np.ndarray, target: np.ndarray) -> float:
+        intensities = self.calc_intensities(kappas)
+        rmse = np.sqrt(np.sum((intensities - target) ** 2)) / len(intensities)
+        curvature = np.sum(np.abs(kappas[0:-2] - 2 * kappas[1:-1] + kappas[2:])) * len(intensities) * 2 * 1e-6
+        low_values = - np.sum(kappas) / len(kappas) * 6e-3
+        return rmse + curvature + low_values
