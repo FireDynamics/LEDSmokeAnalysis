00b489fe69380c39d591f6054e1b87a3a72b8e41
O-Sven <1860830@uni-wuppertal.de>
2021-09-09 15:13:47 +0200

    add typing to led_helper.py

--- ledsa/core/led_helper.py
+++ ledsa/core/led_helper.py
@@ -2,11 +2,14 @@ from ledsa.core._led_helper_functions import *
 from ledsa.core._led_helper_functions_s1 import *
 from ledsa.core._led_helper_functions_s2 import *
 from ledsa.core._led_helper_functions_s3 import *
+from ledsa.core.ledsa_conf import ConfigData
 
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import rawpy
+
+from typing import List
 
 sep = os.path.sep
 
@@ -19,7 +22,7 @@ sep = os.path.sep
 
 # should handle all exception for opening files
 # when exception is thrown, ask if std conf-file should be used or user input
-def load_file(filename, delim=' ', dtype='float', atleast_2d=False, silent=False):
+def load_file(filename: str, delim=' ', dtype='float', atleast_2d=False, silent=False) -> np.ndarray:
     try:
         data = np.loadtxt(filename, delimiter=delim, dtype=dtype)
     except OSError as e:
@@ -41,7 +44,7 @@ def load_file(filename, delim=' ', dtype='float', atleast_2d=False, silent=False
     return np.atleast_1d(data)
 
 
-def read_file(filename, channel, colordepth=14):
+def read_file(filename: str, channel: int, colordepth=14) -> np.ndarray:
     """
     Returns a 2D array of channel values depending on the colordepth.
     8bit is default range for JPG. Bayer array is a 2D array where
@@ -74,7 +77,7 @@ def read_file(filename, channel, colordepth=14):
 # ------------------------------------
 # """
 
-def get_img_name(img_id):
+def get_img_name(img_id: int) -> np.ndarray:
     infos = load_file('.{}analysis{}image_infos_analysis.csv'.format(sep, sep), ',', 'str',
                       silent=True, atleast_2d=True)
     for i in range(infos.shape[0]):
@@ -83,7 +86,7 @@ def get_img_name(img_id):
     raise NameError("Could not find an image name to id {}.".format(img_id))
 
 
-def get_img_id(img_name):
+def get_img_id(img_name: str) -> int:
     infos = load_file('.{}analysis{}image_infos_analysis.csv'.format(sep, sep), ',', 'str',
                       silent=True, atleast_2d=True)
     for i in range(infos.shape[0]):
@@ -92,13 +95,14 @@ def get_img_id(img_name):
     raise NameError("Could not find an image id for {}.".format(img_name))
 
 
-def get_last_img_id():
+def get_last_img_id() -> int:
     infos = load_file('.{}analysis{}image_infos_analysis.csv'.format(sep, sep), ',', 'str',
                       silent=True, atleast_2d=True)
     return int(infos[-1, 0])
 
 
-def get_img_id_from_time(time):
+#TODO: check if time can be int (float comparisson)
+def get_img_id_from_time(time: float) -> int:
     infos = load_file('.{}analysis{}image_infos_analysis.csv'.format(sep, sep), ',', 'str', silent=True)
     for i in range(infos.shape[0]):
         if float(infos[i, 3]) == time:
@@ -106,7 +110,7 @@ def get_img_id_from_time(time):
     raise NameError("Could not find an image id at {}s.".format(time))
 
 
-def get_time_from_img_id(img_id):
+def get_time_from_img_id(img_id: int) -> int:
     infos = load_file('.{}analysis{}image_infos_analysis.csv'.format(sep, sep), ',', 'str', silent=True)
     for i in range(infos.shape[0]):
         if float(infos[i, 0]) == img_id:
@@ -120,7 +124,7 @@ def get_time_from_img_id(img_id):
 # ------------------------------------
 # """
 
-def create_needed_directories(channels):
+def create_needed_directories(channels: List[int]) -> None:
     if not os.path.exists('plots'):
         os.mkdir('plots')
         print("Directory plots created ")
@@ -133,7 +137,7 @@ def create_needed_directories(channels):
             print("Directory analysis{}channel{} created".format(sep, channel))
 
 
-def request_config_parameters(config):
+def request_config_parameters(config: ConfigData) -> None:
     if config['DEFAULT']['time_img'] == 'None' and \
             config['DEFAULT']['exif_time_infront_real_time'] == 'None':
         config.in_time_img()
@@ -158,7 +162,7 @@ def request_config_parameters(config):
         config.save()
 
 
-def generate_image_infos_csv(config, build_experiment_infos=False, build_analysis_infos=False):
+def generate_image_infos_csv(config: ConfigData, build_experiment_infos=False, build_analysis_infos=False) -> None:
     config_switch = []
     if build_experiment_infos:
         config_switch.append('DEFAULT')
@@ -182,7 +186,7 @@ def generate_image_infos_csv(config, build_experiment_infos=False, build_analysi
 # ------------------------------------
 # """
 
-def find_search_areas(image, window_radius=10, skip=10, threshold_factor=0.25):
+def find_search_areas(image: np.ndarray, window_radius=10, skip=10, threshold_factor=0.25) -> np.ndarray:
     print('finding led search areas')
     led_mask = generate_mask_of_led_areas(image, threshold_factor)
     search_areas = find_pos_of_max_col_val_per_area(image, led_mask, skip, window_radius)
@@ -190,7 +194,7 @@ def find_search_areas(image, window_radius=10, skip=10, threshold_factor=0.25):
     return search_areas
 
 
-def add_search_areas_to_plot(search_areas, ax, config):
+def add_search_areas_to_plot(search_areas: np.ndarray, ax: plt.axes, config: ConfigData) -> None:
     for i in range(search_areas.shape[0]):
         ax.add_patch(plt.Circle((search_areas[i, 2], search_areas[i, 1]),
                                 radius=int(config['window_radius']),
@@ -207,14 +211,14 @@ def add_search_areas_to_plot(search_areas, ax, config):
 # ------------------------------------
 # """
 
-def match_leds_to_led_arrays(search_areas, config):
+def match_leds_to_led_arrays(search_areas: np.ndarray, config: ConfigData) -> np.ndarray:
     edge_indices = get_indices_of_outer_leds(config)
     dists_led_arrays_search_areas = calc_dists_between_led_arrays_and_search_areas(edge_indices, search_areas)
     led_arrays = match_leds_to_arrays_with_min_dist(dists_led_arrays_search_areas, edge_indices, config, search_areas)
     return led_arrays
 
 
-def generate_line_indices_files(line_indices):
+def generate_line_indices_files(line_indices: List[int]) -> None:
     for i in range(len(line_indices)):
         out_file = open('analysis{}line_indices_{:03}.csv'.format(sep, i), 'w')
         for iled in line_indices[i]:
@@ -222,7 +226,7 @@ def generate_line_indices_files(line_indices):
         out_file.close()
 
 
-def generate_labeled_led_arrays_plot(line_indices, search_areas):
+def generate_labeled_led_arrays_plot(line_indices: List[int], search_areas: np.ndarray) -> None:
     """plot the labeled LEDs"""
     for i in range(len(line_indices)):
         plt.scatter(search_areas[line_indices[i], 2],
@@ -239,7 +243,8 @@ def generate_labeled_led_arrays_plot(line_indices, search_areas):
 # ------------------------------------
 # """
 
-def generate_analysis_data(img_filename, channel, search_areas, line_indices, conf, fit_leds=True, debug=False, debug_led=None):
+def generate_analysis_data(img_filename: str, channel: int, search_areas: np.ndarray, line_indices: List[int],
+                           conf: ConfigData, fit_leds=True, debug=False, debug_led=None) -> np.ndarray:
     data = read_file('{}{}'.format(conf['img_directory'], img_filename), channel=channel)
     window_radius = int(conf['window_radius'])
     img_analysis_data = []
@@ -259,7 +264,7 @@ def generate_analysis_data(img_filename, channel, search_areas, line_indices, co
     return img_analysis_data
 
 
-def create_fit_result_file(img_data, img_id, channel):
+def create_fit_result_file(img_data: np.ndarray, img_id: int, channel: int) -> None:
     img_infos = load_file('analysis{}image_infos_analysis.csv'.format(sep), dtype='str', delim=',', silent=True,
                           atleast_2d=True)
     root = os.getcwd()
@@ -269,7 +274,7 @@ def create_fit_result_file(img_data, img_id, channel):
     save_results_in_file(channel, img_data, img_filename, img_id, img_infos, root)
     
     
-def create_imgs_to_process_file():
+def create_imgs_to_process_file() -> None:
     image_infos = load_file('.{}analysis{}image_infos_analysis.csv'.format(sep, sep), dtype='str', delim=',',
                             atleast_2d=True)
     img_filenames = image_infos[:, 1]
@@ -285,7 +290,7 @@ def create_imgs_to_process_file():
 # ------------------------------------
 # """
 
-def find_not_analysed_imgs(channel):
+def find_and_save_not_analysed_imgs(channel: int) -> None:
     image_infos = load_file('.{}analysis{}image_infos_analysis.csv'.format(sep, sep), dtype='str', delim=',',
                             atleast_2d=True)
     all_imgs = image_infos[:, 1]
