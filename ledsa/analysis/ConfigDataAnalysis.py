dfcbb3d20ff8bae8fe7c221f2af897b968064512
Kristian <73885850+kriboe90@users.noreply.github.com>
2023-08-17 16:45:39 -0400

    adding docstrings and typehints

--- ledsa/analysis/ConfigDataAnalysis.py
+++ ledsa/analysis/ConfigDataAnalysis.py
@@ -2,12 +2,45 @@ import configparser as cp
 
 
 class ConfigDataAnalysis(cp.ConfigParser):
+    """
+    Class responsible for handling the configuration data related to LEDSA's data analysis.
 
+    """
     def __init__(self, load_config_file=True, camera_position=None, num_of_layers=20, domain_bounds=None,
                  led_arrays=None, num_ref_images=10, camera_channels=0, num_of_cores=1,
                  reference_property='sum_col_val',
                  average_images=False, solver='numeric', weighting_preference=-6e-3, weighting_curvature=1e-6,
                  num_iterations=200):
+        """
+        :param load_config_file: Determines whether to load the config file on initialization. Defaults to True.
+        :type load_config_file: bool
+        :param camera_position: Global X, Y, Z position of the camera. Defaults to None.
+        :type camera_position: list[float] or None
+        :param num_of_layers: Number of horizontal smoke layers. Defaults to 20.
+        :type num_of_layers: int
+        :param domain_bounds: Lower and upper bounds of the computational domain. Defaults to None.
+        :type domain_bounds: list[float] or None
+        :param led_arrays: LED arrays for which the extinction coefficients should be computed. Defaults to None.
+        :type led_arrays: list[int] or None
+        :param num_ref_images: Number of images used to compute normalize LED intensities. Defaults to 10.
+        :type num_ref_images: int
+        :param camera_channels: Camera channels to be considered in the analysis. Defaults to 0. #TODO: check
+        :type camera_channels: int
+        :param num_of_cores: Number of CPU cores for (multicore) processing. If greater than 1, multicore processing is applied. Defaults to 1.
+        :type num_of_cores: int
+        :param reference_property: Property used for reference in LEDSA. Defaults to 'sum_col_val'. #TODO: give options?
+        :type reference_property: str
+        :param average_images: Determines if intensities are computed as an average from two consecutive images. Defaults to False.
+        :type average_images: bool
+        :param solver: Method used to compute extinction coefficients - can be 'linear' or 'numeric'. Defaults to 'numeric'. #TODO: hint analytic is beta now
+        :type solver: str
+        :param weighting_preference: Weighting preference option for the numeric solver. Defaults to -6e-3. #TODO: specify
+        :type weighting_preference: float
+        :param weighting_curvature: Weighting curvature option for the numeric solver. Defaults to 1e-6.  #TODO: specify
+        :type weighting_curvature: float
+        :param num_iterations: Number of iterations for the numeric solver. Defaults to 200.
+        :type num_iterations: int
+        """
         cp.ConfigParser.__init__(self, allow_no_value=True)
         if load_config_file:
             self.load()
@@ -46,7 +79,13 @@ class ConfigDataAnalysis(cp.ConfigParser):
                 self.write(configfile)
             print('config_analysis.ini created')
 
-    def load(self):
+    def load(self) -> None:
+        """
+        Loads the configuration data from 'config_analysis.ini' file.
+
+        Raises:
+            FileNotFoundError: If 'config_analysis.ini' is not found in the working directory.
+        """
         try:
             self.read_file(open('config_analysis.ini'))
         except FileNotFoundError:
@@ -55,34 +94,64 @@ class ConfigDataAnalysis(cp.ConfigParser):
             exit(1)
         print('config_analysis.ini loaded!')
 
-    def save(self):
+    def save(self) -> None:
+        """
+        Saves the current configuration to 'config_analysis.ini' file.
+        """
         with open('config_analysis.ini', 'w') as configfile:
             self.write(configfile)
         print('config_analysis.ini saved')
 
-    def get_list_of_values(self, section, option, dtype=int):
+    def get_list_of_values(self, section:str, option:str, dtype=int) -> None:
+        """
+        Returns a list of values of a specified dtype from a given section and option.
+
+        :param section: Section in the configuration file.
+        :type section: str
+        :param option: Option under the specified section.
+        :type option: str
+        :param dtype: Data type of the values to be returned. Defaults to int.
+        :type dtype: type
+        :return: List of values or None if the option's value is 'None'.
+        :rtype: list or None
+        """
         if self[section][option] == 'None':
             return None
         values = [dtype(i) for i in self[section][option].split()]
         return values
 
-    def in_camera_channels(self):
+    def in_camera_channels(self) -> None:
+        """
+        Prompts the user to input the camera channels and updates the configuration.
+        """
         self['DEFAULT']['camera_channels'] = input('Please give the camera channels that should be considered in the '
                                                    'analysis: ')
 
-    def in_camera_position(self):
+    def in_camera_position(self) -> None:
+        """
+        Prompts the user to input the camera's global X, Y, Z coordinates and updates the configuration.
+        """
         self['experiment_geometry']['camera_position'] = input('Please give the global X Y Z [m] coordinates of the '
                                                                'camera : ')
 
-    def in_num_of_layers(self):
+    def in_num_of_layers(self) -> None:
+        """
+        Prompts the user to input the number of layers for spatial domain discretization and updates the configuration.
+        """
         self['model_parameters']['num_of_layers'] = input('Please give number of layers the spatial domain should '
                                                           'be discretized to : ')
 
-    def in_domain_bounds(self):
+    def in_domain_bounds(self) -> None:
+        """
+        Prompts the user to input the lower and upper height of the spatial domain and updates the configuration.
+        """
         self['model_parameters']['domain_bounds'] = input('Please give lower and upper height [m] of the spatial '
                                                           'domain : ')
 
-    def in_led_arrays(self):
+    def in_led_arrays(self) -> None:
+        """
+        Prompts the user to input the IDs of (merged) LED Arrays for computation and updates the configuration.
+        """
         self['model_parameters']['led_arrays'] = input('Please give IDs of (merged) LED Arrays to compute: ')
 
 
