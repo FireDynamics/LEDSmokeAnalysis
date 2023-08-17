f1f1182f03396ad2f38cd84efc0c24caa4152ac0
Kristian <73885850+kriboe90@users.noreply.github.com>
2023-08-17 16:47:12 -0400

    adding docstrings and typehints

--- ledsa/data_extraction/init_functions.py
+++ ledsa/data_extraction/init_functions.py
@@ -8,6 +8,12 @@ from ledsa.core.ConfigData import ConfigData
 
 
 def create_needed_directories(channels: List[int]) -> None:
+    """
+    Create required directories for storing plots and analysis results.
+
+    :param channels: List of channel indices to create subdirectories under 'analysis'.
+    :type channels: List[int]
+    """
     if not os.path.exists('plots'):
         os.mkdir('plots')
         print("Directory plots created ")
@@ -22,6 +28,12 @@ def create_needed_directories(channels: List[int]) -> None:
 
 
 def request_config_parameters(config: ConfigData) -> None:
+    """
+    Prompt and update config parameters if they are not set.
+
+    :param config: Configuration data object that contains settings.
+    :type config: ConfigData
+    """
     if config['DEFAULT']['img_directory'] == 'None':
         config.in_img_dir()
         config.save()
@@ -47,6 +59,17 @@ def request_config_parameters(config: ConfigData) -> None:
 
 
 def generate_image_infos_csv(config: ConfigData, build_experiment_infos=False, build_analysis_infos=False) -> None:
+    """
+    Generate CSV files with image information for experiment and/or analysis.
+    Contains image name, exif time and experiment time
+
+    :param config: Configuration data object.
+    :type config: ConfigData
+    :param build_experiment_infos: Whether to build CSV for experiment info.
+    :type build_experiment_infos: bool
+    :param build_analysis_infos: Whether to build CSV for analysis info.
+    :type build_analysis_infos: bool
+    """
     config_switch = []
     if build_experiment_infos:
         config_switch.append('DEFAULT')
@@ -67,7 +90,21 @@ def generate_image_infos_csv(config: ConfigData, build_experiment_infos=False, b
             _save_analysis_infos(img_data)
 
 
-def _calc_experiment_and_real_time(build_type, config, tag, img_number):
+def _calc_experiment_and_real_time(build_type: str, config: ConfigData, tag: str, img_number: int) -> None:
+    """
+    Calculate experiment and real-time based on image metadata and config settings.
+
+    :param build_type: Type of build process (either 'DEFAULT' or 'analyse_photo').
+    :type build_type: str
+    :param config: Configuration data object.
+    :type config: ConfigData
+    :param tag: Metadata tag to extract time information from image.
+    :type tag: str
+    :param img_number: Image ID where the time should be read from.
+    :type img_number: int
+    :return: Tuple containing experiment time and real time.
+    :rtype: tuple
+    """
     exif_entry = get_exif_entry(config['DEFAULT']['img_directory'] +
                                 config['DEFAULT']['img_name_string'].format(int(img_number)), tag)
     date, time_meta = exif_entry.split(' ')
@@ -82,7 +119,17 @@ def _calc_experiment_and_real_time(build_type, config, tag, img_number):
     return experiment_time, time
 
 
-def _get_datetime_from_str(date, time):
+def _get_datetime_from_str(date: str, time: str) -> datetime:
+    """
+    Convert date and time strings to a datetime object.
+
+    :param date: Date string in the format '%Y:%m:%d' or '%d.%m.%Y'.
+    :type date: str
+    :param time: Time string in the format '%H:%M:%S'.
+    :type time: str
+    :return: Corresponding datetime object.
+    :rtype: datetime.datetime
+    """
     if date.find(":") != -1:
         date_time = datetime.strptime(date + ' ' + time, '%Y:%m:%d %H:%M:%S')
     else:
@@ -90,7 +137,21 @@ def _get_datetime_from_str(date, time):
     return date_time
 
 
-def _find_img_number_list(first, last, increment, number_string_length=4):
+def _find_img_number_list(first: int, last: int, increment: int, number_string_length=4) -> List[str]:
+    """
+    Generate a list of image numbers with specified increment and padding.
+
+    :param first: First image ID.
+    :type first: int
+    :param last: Last image ID.
+    :type last: int
+    :param increment: Increment value for image ID range.
+    :type increment: int
+    :param number_string_length: Number of digits in the image ID (default is 4).
+    :type number_string_length: int
+    :return: List of image IDs as strings.
+    :rtype: List[str]
+    """
     if last >= first:
         return [str(ele).zfill(number_string_length) for ele in range(first, last + 1, increment)]
 
@@ -104,7 +165,17 @@ def _find_img_number_list(first, last, increment, number_string_length=4):
     return num_list
 
 
-def _build_img_data_string(build_type, config):
+def _build_img_data_string(build_type: str, config: ConfigData) -> str:
+    """
+    Construct image data string for CSV generation.
+
+    :param build_type: Type of build process (either 'DEFAULT' or 'analyse_photo').
+    :type build_type: str
+    :param config: Configuration data object.
+    :type config: ConfigData
+    :return: Formatted image data string.
+    :rtype: str
+    """
     img_data = ''
     img_idx = 1
     if config['analyse_photo']['first_img'] == 'None':
@@ -128,7 +199,13 @@ def _build_img_data_string(build_type, config):
     return img_data
 
 
-def _save_analysis_infos(img_data):
+def _save_analysis_infos(img_data: str) -> None:
+    """
+    Save the image data for analysis to a CSV file.
+
+    :param img_data: Formatted string containing image data.
+    :type img_data: str
+    """
     file_path = os.path.join('analysis', 'image_infos_analysis.csv')
     out_file = open(file_path, 'w')
     out_file.write("#ID,Name,Time[s],Experiment_Time[s]\n")
@@ -136,7 +213,13 @@ def _save_analysis_infos(img_data):
     out_file.close()
 
 
-def _save_experiment_infos(img_data):
+def _save_experiment_infos(img_data: str) -> None:
+    """
+    Save the image data for experiment to a CSV file.
+
+    :param img_data: Formatted string containing image data.
+    :type img_data: str
+    """
     out_file = open('image_infos.csv', 'w')
     out_file.write("#Count,Name,Time[s],Experiment_Time[s]\n")
     out_file.write(img_data)
