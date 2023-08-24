f6dce57497d5102db33afdb63da4588258fa4965
Kristian <73885850+kriboe90@users.noreply.github.com>
2023-08-24 17:43:27 -0400

    outsource parser_arguments_declaration.py arguments and run functions in separate files and merge __main__.py files

--- ledsa/__main__.py
+++ ledsa/__main__.py
@@ -1,12 +1,12 @@
 # this file is executed if the package is run via python -m ledsa [arguments]
-import os
 from typing import List
 import argparse
 import sys
 
-from .data_extraction.DataExtractor import DataExtractor
-from .core.ConfigData import ConfigData
-from ledsa.analysis.__main__ import add_parser_argument_analysis, run_analysis_arguments_with_extinction_coefficient
+from ledsa.parser_arguments_declaration import add_parser_arguments_data_extraction, add_parser_arguments_testing, \
+    add_parser_arguments_demo, add_parser_argument_analysis
+from ledsa.parser_arguments_run import run_data_extraction_arguments, run_testing_arguments, run_demo_arguments, \
+    run_analysis_arguments, run_analysis_arguments_with_extinction_coefficient
 
 
 def main(argv: List[str]) -> None:
@@ -36,190 +36,28 @@ def main(argv: List[str]) -> None:
         run_demo_arguments(args, parser)
     else:
         run_data_extraction_arguments(args)
+        run_analysis_arguments(args)
         run_analysis_arguments_with_extinction_coefficient(args)
         run_testing_arguments(args)
 
-
-def add_parser_arguments_data_extraction(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
+def apply_cc_on_ref_property(ex_data) -> None:
     """
-    Add parser arguments related to data extraction.
+    Apply color correction on the reference property and save it in the binary as column {ref_property}_cc.
 
-    :param parser: ArgumentParser object to which the arguments are added.
-    :type parser: argparse.ArgumentParser
-    :return: Modified ArgumentParser object with added arguments.
-    :rtype: argparse.ArgumentParser
+    :param ex_data: Experiment data containing the reference property
+    :type ex_data: ExperimentData
     """
-    parser.add_argument('-s1', '--step_1', '--find_search_areas', action='store_true',
-                        help='STEP1: analyse a reference image to find the LED positions and their labels')
-    parser.add_argument('-s2', '-step_2', '--analyse_positions', action='store_true',
-                        help='STEP2: finds the LED array to which each LED belongs')
-    parser.add_argument('-s3', '--step_3', '--analyse_photo', action='store_true',
-                        help='STEP3: finds the changes in light intensity')
-    parser.add_argument('-s3_fast', '--step_3_fast', '--analyse_photo_fast', action='store_true',
-                        help='Step 3 but without the fits.')
-    parser.add_argument('-conf', '--config', nargs='*', default=None,
-                        help='creates the default configuration file. optional arguments are are: img_directory, '
-                             'reference_img, number_of_cores.')
-    parser.add_argument('-re', '--restart', action='store_true',
-                        help='Restarts step 3 of the analysis after the program was terminated before it finished.')
-    parser.add_argument('-r', '--red', action='store_true',
-                        help='Use the red channel for step3. Default value.')
-    parser.add_argument('-g', '--green', action='store_true',
-                        help='Use the green channel for step3')
-    parser.add_argument('-b', '--blue', action='store_true',
-                        help='Use the blue channel for step3.')
-    parser.add_argument('-rgb', '--rgb', action='store_true',
-                        help='Run step3 for each channel.')
-    parser.add_argument('-coord', '--coordinates', action='store_true',
-                        help='Calculates the 3D coordinates from the coordinates given in the configfile and the '
-                             'reference image.')
-    return parser
-
-
-def add_parser_arguments_testing(parser: argparse.ArgumentParser) -> argparse.ArgumentParser: #TODO: Revise arguments
-    """
-    Add parser arguments related to testing.
-
-    :param parser: ArgumentParser object to which the arguments are added.
-    :type parser: argparse.ArgumentParser
-    :return: Modified ArgumentParser object with added arguments.
-    :rtype: argparse.ArgumentParser
-    """
-    parser.add_argument('-atest', '--acceptance_test', action='store_true',
-                        help='Runs the acceptance test suit')
-    parser.add_argument('-atest_debug', '--acceptance_test_debug', action='store_true',
-                        help='Runs acceptance test suit in debug mode')
-    return parser
-
-
-def add_parser_arguments_demo(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
-    """
-    Add parser arguments related to running a demo.
-
-    :param parser: ArgumentParser object to which the arguments are added.
-    :type parser: argparse.ArgumentParser
-    :return: Modified ArgumentParser object with added arguments.
-    :rtype: argparse.ArgumentParser
-    """
-    parser.add_argument('-d', '--demo' , action='store_true',
-                        help='Flag to indicate that the LEDSA demo should run. Must be used with --setup or --run.')
-    parser.add_argument('--setup', type=str,
-                        help='Path for the LEDSA demo setup. Use with --demo.')
-    parser.add_argument('--run', action='store_true',
-                        help='Flag to indicate that the LEDSA demo should run. Must be used with --setup.')
-    parser.add_argument('--n_cores', type=int,
-                        help='Flag to indicate that the LEDSA demo should run. Must be used with --run.')
-
-    return parser
-
-
-def run_data_extraction_arguments(args: argparse.Namespace) -> None:
-    """
-    Execute actions based on data extraction arguments.
-
-    :param args: Parsed command line arguments.
-    :type args: argparse.Namespace
-    """
-    if args.config is not None: # TODO: remove additional options from config parser argument
-        if len(args.config) == 0:
-            ConfigData(load_config_file=False)
-        if len(args.config) == 1:
-            ConfigData(load_config_file=False, img_directory=args.config[0])
-        if len(args.config) == 2:
-            ConfigData(load_config_file=False, img_directory=args.config[0], reference_img=args.config[1])
-        if len(args.config) == 3:
-            ConfigData(load_config_file=False, img_directory=args.config[0], reference_img=args.config[1],
-                       num_of_cores=args.config[2])
-
-    channels = []
-    if args.rgb:
-        channels = [0, 1, 2]
-    else:
-        if args.r:
-            channels.append(0)
-        if args.g:
-            channels.append(1)
-        if args.b:
-            channels.append(2)
-    if len(channels) == 0:
-        channels.append(0)
-
-    if args.r or args.g or args.b or args.rgb and not args.s3_fast:
-        args.s3 = True
-
-    if args.s1 or args.s2:
-        de = DataExtractor(build_experiment_infos=False, channels=channels)
-        if args.s1:
-            de.find_search_areas(de.config['find_search_areas']['reference_img'])
-            de.plot_search_areas(de.config['find_search_areas']['reference_img'])
-        if args.s2:
-            de.match_leds_to_led_arrays()
-
-    if args.s3:
-        de = DataExtractor(build_experiment_infos=True, channels=channels)
-        de.setup_step3()
-        de.process_image_data()
-
-    if args.s3_fast:
-        de = DataExtractor(build_experiment_infos=True, channels=channels, fit_leds=False)
-        de.setup_step3()
-        de.process_image_data()
-
-    if args.re:
-        channels = [0, 1, 2]  # TODO: just for testing
-        de = DataExtractor(build_experiment_infos=False, channels=channels, fit_leds=False)
-        de.setup_restart()
-        de.process_image_data()
-
-    if args.coordinates:
-        from ledsa.ledpositions import calculate_coordinates
-        calculate_coordinates()
-
-
-def run_testing_arguments(args) -> None:
-    """
-    Execute actions based on testing arguments.
-
-    :param args: Parsed command line arguments.
-    :type args: argparse.Namespace
-    """
-
-    if args.atest:
-        from ledsa.tests.AcceptanceTests.__main__ import main
-        main()
-
-    if args.atest_debug:
-        from ledsa.tests.AcceptanceTests.__main__ import main # TODO: should import be relative or absolut?
-        main(extended_logs=True)
-
-
-def run_demo_arguments(args: argparse.Namespace, parser: argparse.ArgumentParser) -> None:
-    """
-    Execute actions based on demo arguments.
-
-    :param args: Parsed command line arguments.
-    :type args: argparse.Namespace
-    """
-    if not args.setup and not args.run:
-        parser.error("--demo requires either --setup or --run argument.")
-    if args.n_cores and not args.run:
-        parser.error("--n_cores requires the --run argument.")
-
-    if args.setup:
-        src_path = '/Users/kristianboerger/working_files/ledsa_demo_src'
-        image_src_path = os.path.join(src_path, 'image_data_V001_Cam01.zip')
-        config_src_path = os.path.join(src_path, 'config')
-        from .demo.demo_setup import setup_demo
-        setup_demo(destination_path=args.setup, image_src_path=image_src_path, config_src_path=config_src_path)
-    if args.run:
-        from .demo.demon_run import run_demo
-        if args.n_cores:
-            run_demo(num_of_cores=args.n_cores)
-        else:
-            run_demo()
-
-
+    import numpy as np
+    from ledsa.analysis.data_preparation import apply_color_correction
+    try:
+        cc_matrix = np.genfromtxt('mean_all_cc_matrix_integral.csv', delimiter=',')
+    except FileNotFoundError:
+        print('File: mean_all_cc_matrix_integral.csv containing the color correction matrix not found')
+        exit(1)
+    apply_color_correction(cc_matrix, on=ex_data.reference_property, channels=args.cc_channels)
 
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+
+
---
+++ ledsa/parser_arguments_run.py
@@ -0,0 +1,176 @@
+import argparse
+import os
+
+from ledsa.analysis import ExtinctionCoefficientsNumeric as ECN
+from ledsa.analysis.ConfigDataAnalysis import ConfigDataAnalysis
+from ledsa.analysis.Experiment import Experiment
+from ledsa.analysis.ExperimentData import ExperimentData
+# from ledsa.analysis.__main__ import apply_cc_on_ref_property
+
+from ledsa.core.ConfigData import ConfigData
+from ledsa.data_extraction.DataExtractor import DataExtractor
+
+
+def run_data_extraction_arguments(args: argparse.Namespace) -> None:
+    """
+    Execute actions based on data extraction arguments.
+
+    :param args: Parsed command line arguments.
+    :type args: argparse.Namespace
+    """
+    if args.config is not None: # TODO: remove additional options from config parser argument
+        if len(args.config) == 0:
+            ConfigData(load_config_file=False)
+        if len(args.config) == 1:
+            ConfigData(load_config_file=False, img_directory=args.config[0])
+        if len(args.config) == 2:
+            ConfigData(load_config_file=False, img_directory=args.config[0], reference_img=args.config[1])
+        if len(args.config) == 3:
+            ConfigData(load_config_file=False, img_directory=args.config[0], reference_img=args.config[1],
+                       num_of_cores=args.config[2])
+
+    channels = []
+    if args.rgb:
+        channels = [0, 1, 2]
+    else:
+        if args.red:
+            channels.append(0)
+        if args.green:
+            channels.append(1)
+        if args.blue:
+            channels.append(2)
+    if len(channels) == 0:
+        channels.append(0)
+
+    if args.red or args.green or args.blue or args.rgb and not args.step_3_fast:
+        args.step_3 = True
+
+    if args.step_1 or args.step_2:
+        de = DataExtractor(build_experiment_infos=False, channels=channels)
+        if args.step_1:
+            de.find_search_areas(de.config['find_search_areas']['reference_img'])
+            de.plot_search_areas(de.config['find_search_areas']['reference_img'])
+        if args.step_2:
+            de.match_leds_to_led_arrays()
+
+    if args.step_3:
+        de = DataExtractor(build_experiment_infos=True, channels=channels)
+        de.setup_step3()
+        de.process_image_data()
+
+    if args.step_3_fast:
+        de = DataExtractor(build_experiment_infos=True, channels=channels, fit_leds=False)
+        de.setup_step3()
+        de.process_image_data()
+
+    if args.restart:
+        channels = [0, 1, 2]  # TODO: just for testing
+        de = DataExtractor(build_experiment_infos=False, channels=channels, fit_leds=False)
+        de.setup_restart()
+        de.process_image_data()
+
+    if args.coordinates:
+        from ledsa.ledpositions.coordinates import calculate_coordinates
+        calculate_coordinates()
+
+
+def run_testing_arguments(args) -> None:
+    """
+    Execute actions based on testing arguments.
+
+    :param args: Parsed command line arguments.
+    :type args: argparse.Namespace
+    """
+
+    if args.acceptance_test:
+        from ledsa.tests.AcceptanceTests.__main__ import main
+        main()
+
+    if args.acceptance_test_debug:
+        from ledsa.tests.AcceptanceTests.__main__ import main # TODO: should import be relative or absolut?
+        main(extended_logs=True)
+
+
+def run_demo_arguments(args: argparse.Namespace, parser: argparse.ArgumentParser) -> None:
+    """
+    Execute actions based on demo arguments.
+
+    :param args: Parsed command line arguments.
+    :type args: argparse.Namespace
+    """
+    if not args.setup and not args.run:
+        parser.error("--demo requires either --setup or --run argument.")
+    if args.n_cores and not args.run:
+        parser.error("--n_cores requires the --run argument.")
+
+    if args.setup:
+        src_path = '/Users/kristianboerger/working_files/ledsa_demo_src'
+        image_src_path = os.path.join(src_path, 'image_data_V001_Cam01.zip')
+        config_src_path = os.path.join(src_path, 'config')
+        from .demo.demo_setup import setup_demo
+        setup_demo(destination_path=args.setup, image_src_path=image_src_path, config_src_path=config_src_path)
+    if args.run:
+        from .demo.demo_run import run_demo
+        if args.n_cores:
+            run_demo(num_of_cores=args.n_cores)
+        else:
+            run_demo()
+
+def run_analysis_arguments(args) -> None:
+    """
+    Handle the configuration and preprocessing based on the command line arguments.
+
+    :param args: Parsed command line arguments
+    :type args: argparse.Namespace
+    """
+    if args.config_analysis is not None:
+        ConfigDataAnalysis(load_config_file=False)
+
+    if args.cc:
+        ex_data = ExperimentData()
+        apply_cc_on_ref_property(ex_data)
+
+
+def run_analysis_arguments_with_extinction_coefficient(args) -> None:
+    """
+    Run the extinction coefficient calculation based on the command line arguments.
+
+    :param args: Parsed command line arguments
+    :type args: argparse.Namespace
+    """
+    run_analysis_arguments(args)
+    if args.analysis:
+        extionction_coefficient_calculation(args)
+
+
+def extionction_coefficient_calculation(args) -> None:
+    """
+    Calculate extinction coefficients and save the results to a file.
+
+    :param args: Parsed command line arguments
+    :type args: argparse.Namespace
+    """
+    ex_data = ExperimentData()
+    ex_data.request_config_parameters()
+    for array in ex_data.led_arrays:
+        for channel in ex_data.channels:
+            out_file = os.path.join(os.getcwd(), 'analysis', 'AbsorptionCoefficients',
+                                    f'absorption_coefs_numeric_channel_{channel}_{ex_data.reference_property}_led_array_{array}.csv')
+            if not os.path.exists(out_file):
+                ex = Experiment(layers=ex_data.layers, led_array=array, camera=ex_data.camera, channel=channel,
+                                merge_led_arrays=ex_data.merge_led_arrays)
+                eca = ECN.ExtinctionCoefficientsNumeric(ex, reference_property=ex_data.reference_property,
+                                                        num_ref_imgs=ex_data.num_ref_images,
+                                                        weighting_curvature=ex_data.weighting_curvature,
+                                                        weighting_preference=ex_data.weighting_preference,
+                                                        num_iterations=ex_data.num_iterations)
+                if ex_data.n_cpus > 1:
+                    print(f"Calculation of extinction coefficients runs on {ex_data.n_cpus} cpus!")
+                    eca.calc_and_set_coefficients_mp(ex_data.n_cpus)
+                else:
+                    print("Calculation of extinction coefficients runs on a single cpu!")
+                    eca.calc_and_set_coefficients()
+                eca.save()
+                print(f"{out_file} created!")
+            else:
+                print(f"{out_file} already exists!")
