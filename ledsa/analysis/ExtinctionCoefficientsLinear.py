eda99ce387241acc9bb43661504b84d3e75d3283
Kristian <boerger.k@icloud.com>
2025-05-21 14:23:40 +0200

    add experimental function for linear extinction coefficients computation

--- ledsa/analysis/ExtinctionCoefficientsLinear.py
+++ ledsa/analysis/ExtinctionCoefficientsLinear.py
@@ -4,7 +4,6 @@ from scipy.optimize import nnls
 from ledsa.analysis.Experiment import Experiment, Layers, Camera
 from ledsa.analysis.ExtinctionCoefficients import ExtinctionCoefficients
 
-
 class ExtinctionCoefficientsLinear(ExtinctionCoefficients):
     """
     ExtinctionCoefficientsLinear computes the extinction coefficients by
@@ -87,3 +86,61 @@ class ExtinctionCoefficientsLinear(ExtinctionCoefficients):
         sigmas, residuals = nnls(A_aug, b_aug)
 
         return sigmas
+
+    # For investigation
+    # import os
+
+    # def calc_coefficients_of_img(self, rel_intensities: np.ndarray) -> np.ndarray:
+    #     """
+    #     Calculate the extinction coefficients for a single image using a linearized approach.
+    #     This method solves the linear system derived from the Beer-Lambert law using
+    #     non-negative least squares with Tikhonov regularization.
+    #
+    #     :param rel_intensities: Array of relative (normalized) LED intensities (I_e/I_0).
+    #     :type rel_intensities: np.ndarray
+    #     :return: Array of the computed extinction coefficients (sigmas).
+    #     :rtype: np.ndarray
+    #     """
+    #     # Avoid log(0) by clipping intensities to a small positive value
+    #     eps = 1e-10
+    #     target = np.clip(rel_intensities, eps, 1.0)
+    #
+    #     # Compute the optical depth vector: b = -ln(I_e/I_0)
+    #     # This linearizes the Beer-Lambert law: I_e/I_0 = exp(-sum(sigma_i * distance_i))
+    #     optical_depths = -np.log(target)
+    #
+    #     # Get dimensions of the problem
+    #     n_leds, n_layers = self.distances_per_led_and_layer.shape
+    #
+    #     # Build finite-difference matrix L for second derivative (curvature penalty)
+    #     # This matrix approximates the second derivative and is used for regularization
+    #     # to enforce smoothness in the solution
+    #     if n_layers >= 3:
+    #         L = np.zeros((n_layers - 2, n_layers))
+    #         for i in range(n_layers - 2):
+    #             # Coefficients for second-order finite difference approximation
+    #             L[i, i] = 1
+    #             L[i, i + 1] = -2
+    #             L[i, i + 2] = 1
+    #     else:
+    #         # Fallback: use an identity if there aren't enough layers
+    #         L = np.eye(n_layers)
+    #
+    #     # Interate lambda_reg from 1e-4 to 1
+    #     logspace = np.logspace(-4, 0, 50)
+    #
+    #     os.chdir('/Users/kristian/Documents/NextVis/25_ledsa/lambda_iteration')
+    #     for lambda_reg in logspace:
+    #         sqrt_lambda = np.sqrt(lambda_reg)
+    #
+    #         # Augment the system with regularization
+    #         # A_aug = [A; sqrt(lambda)*L], where A is the distance matrix
+    #         A_aug = np.vstack((self.distances_per_led_and_layer, sqrt_lambda * L))
+    #
+    #         # Augment the right-hand side: b_aug = [b; 0]
+    #         b_aug = np.hstack((optical_depths, np.zeros(L.shape[0])))
+    #
+    #         # Solve the non-negative least squares problem: min ||A_aug*x - b_aug||^2 subject to x >= 0
+    #         sigmas, residuals = nnls(A_aug, b_aug)
+    #         np.savetxt(f'lambda_{lambda_reg:.4f}.txt', sigmas)
+    #     return sigmas
