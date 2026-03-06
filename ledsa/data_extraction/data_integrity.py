20d219a920a96baffa0d0f5845d9c5fd62df03f8
Kristian <boerger.k@icloud.com>
2026-03-06 11:40:31 +0100

    add docstrings and typehints for data integrity check

--- ledsa/data_extraction/data_integrity.py
+++ ledsa/data_extraction/data_integrity.py
@@ -1,6 +1,26 @@
 import warnings
 
-def check_intensity_normalization(ref_img_data, ref_intensities, reference_property, tolerance=0.05):
+import pandas as pd
+
+
+def check_intensity_normalization(
+        ref_img_data: pd.DataFrame,
+        ref_intensities: pd.Series,
+        reference_property: str,
+        tolerance: float = 0.05
+) -> None:
+    """
+    Checks if the intensity normalization is within the specified tolerance.
+
+    :param ref_img_data: DataFrame containing reference image data.
+    :type ref_img_data: pd.DataFrame
+    :param ref_intensities: Series containing reference intensities.
+    :type ref_intensities: pd.Series
+    :param reference_property: The property to check for normalization.
+    :type reference_property: str
+    :param tolerance: The accepted tolerance for relative deviation.
+    :type tolerance: float
+    """
     rel_devs = (ref_img_data[reference_property] - ref_intensities).abs() / ref_intensities.abs()
     hits = rel_devs[rel_devs > tolerance]
 
@@ -13,9 +33,9 @@ def check_intensity_normalization(ref_img_data, ref_intensities, reference_prope
     ]
 
     msg = (
-        f"In the process of normalisation {len(hits)} values exceed {tolerance:.0%} tolerance of relative deviation"
-        f" against mean intensities! You might check the reference images.\n"
-        + "\n".join(lines)
+            f"In the process of normalisation {len(hits)} values exceed {tolerance:.0%} tolerance of relative deviation"
+            f" against mean intensities! You might check the reference images.\n"
+            + "\n".join(lines)
     )
 
     warnings.warn(msg, category=UserWarning, stacklevel=2)
